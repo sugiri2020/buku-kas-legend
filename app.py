ff9e75ff5b0bc036c249f842b0522c7a712ced2a
sugiri2020 <sugiri2020@gmail.com>
2025-07-26 20:01:42 +0700

    Fix listen 0.0.0.0 and PORT for Railway

--- app.py
+++ app.py
@@ -260,4 +260,6 @@ def logout():
 
 # ---------------------- RUN ----------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Railway memberi PORT dari environment
+    app.run(host='0.0.0.0', port=port, debug=True)
+
